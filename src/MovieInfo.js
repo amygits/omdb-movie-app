ded2651d089214f2157582b056a1b554b60bbb2a
Amy Ma <amyma1@asu.edu>
2021-06-19 23:38:50 -0700

    Fix results when submitting different drop down results

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import './MovieInfo.css';
 import Movies from './Movies';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-
 
 function MovieInfo() {
     const [movies, setMovies] = useState([]);
@@ -23,59 +20,85 @@ function MovieInfo() {
         var getChoice = document.getElementById("filterList");
         var choice = getChoice.options[getChoice.selectedIndex].text;
         console.log(choice);
+
         if (choice === '20') {
             setShow20(true);
             setShow10(false);
             setShow30(false);
-            console.log(show30);
         }
+
         if (choice === '10') {
             setShow20(false);
             setShow10(true);
             setShow30(false);
-            console.log(show30);
         }
+
         if (choice === '30') {
             setShow20(false);
             setShow10(false);
             setShow30(true);
-            console.log(show30);
         }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
+        var blankArr = [];
+
         async function fetchMyAPI() {
            setCurrentPage(1);
            const searchParam = encodeURIComponent(query);
            setQThrow(searchParam);
-           const apiUrl = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json&page=${currentPage}`;
-           console.log(apiUrl);
-           let response = await fetch(apiUrl);
+
+           const apiUrl1 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json&page=${currentPage}`;
+           const apiUrl2 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json&page=${nextPage}`;
+           const apiUrl3 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json&page=${nextPage + 1}`;
+
+           let response = await fetch(apiUrl1);
+           let response2 = await fetch(apiUrl2);
+           let response3 = await fetch(apiUrl3);
+
+           if (response2.ok) {
+                setNextExists(true);
+            }
+
            if (show10) {
                 response = await response.json();
+                console.log(apiUrl1);
                 console.log(response.Search);
-                setMovies(response.Search);
-                }
-           const apiUrl2 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&type=movie&r=json&page=${nextPage}`;
-           let response2 = await fetch(apiUrl2);
-           if (response2.ok) {
-            setNextExists(true);
-            response2 = await response2.json();
-            if (show20) {
-                const arr = movies.concat(response2.Search);
+                const arr = blankArr.concat(response.Search);
+                setMovies(arr);
+           }
+           if (show20 && response2.ok) {
+                response = await response.json();
+                response2 = await response2.json();
+                console.log(apiUrl1);
+                console.log(response.Search);
+                console.log(apiUrl2);
+                console.log(response2.Search);
+                const arr = blankArr.concat(response.Search, response2.Search);
                 console.log(arr);
                 setMovies(arr);
-                setCurrentPage(currentPage + 2);
-                setNextPage(nextPage + 2);
             }
-           }
+            if (show30 && response3.ok) {
+                console.log(apiUrl1);
+                console.log(apiUrl2);
+                console.log(apiUrl3);
+                response = await response.json();
+                response2 = await response2.json();
+                response3 = await response3.json();
+                console.log(response.Search);
+                console.log(response2.Search);
+                console.log(response3.Search);
+                const arr = blankArr.concat(response.Search, response2.Search, response3.Search);
+                console.log(arr);
+                setMovies(arr);
+            }
         }
 
         fetchMyAPI();
         setShowMovies(true);
         setQuery("");
-
+        setMovies([]);
     }
 
     function handleNext() {
@@ -84,31 +107,43 @@ function MovieInfo() {
             console.log(apiUrl);
             let response = await fetch(apiUrl);
             if (response.ok) {
-                response = await response.json();
-                setShowMovies(true);
-                console.log(response.Search);
-                setMovies(response.Search);
-                const apiUrl2 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${qThrow}&r=json&type=movie&page=${prevPage}`;
-                           let response2 = await fetch(apiUrl2);
-                           if (response2.ok) {
-                                setPrevExists(true);
-                                if (show20) {
-                                    const arr = movies.concat(response2.Search);
-                                    console.log(arr);
-                                    setMovies(arr);
-                                    setCurrentPage(currentPage + 2);
-                                    setNextPage(nextPage + 2);
-                                }
-                           }
+                if (show10) {
+                    response = await response.json();
+                    console.log(response.Search);
+                    setMovies(response.Search);
+                    setPrevPage(currentPage);
+                    setCurrentPage(nextPage);
+                    setNextPage(nextPage + 1);
+                    const apiUrl2 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${qThrow}&r=json&type=movie&page=${prevPage}`;
+                    let response2 = await fetch(apiUrl2);
+                    if (response2.ok) {
+                        setPrevExists(true);
+                    }
+                }
+                if (show20) {
+                 response = await response.json();
+                 console.log(response.Search);
+                 setMovies(response.Search);
+                 var temp = currentPage + 1;
+                 setPrevPage(currentPage);
+                 setCurrentPage(currentPage + 2);
+                 setNextPage(nextPage + 4);
+                 const apiUrl2 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${qThrow}&r=json&type=movie&page=${prevPage}`;
+                 let response2 = await fetch(apiUrl2);
+                 if (response2.ok) {
+                    setPrevExists(true);
+                 }
+                 const apiUrl3 = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${qThrow}&r=json&type=movie&page=${temp}`;
+                 let response3 = await fetch(apiUrl3);
+                 const arr = movies.concat(response3.Search);
+                 console.log(arr);
+                 setMovies(arr);
+
+                    }
             }
         }
-
         fetchMyAPI();
         setShowMovies(true);
-        setPrevPage(currentPage);
-        setCurrentPage(nextPage);
-        setNextPage(nextPage + 1);
-
     }
 
     function handlePrev() {
@@ -118,7 +153,6 @@ function MovieInfo() {
                 let response = await fetch(apiUrl);
                 if (response.ok) {
                     response = await response.json();
-                    setShowMovies(true);
                     console.log(response.Search);
                     setMovies(response.Search);
                     setNextPage(prevPage + 1);
@@ -130,7 +164,6 @@ function MovieInfo() {
             fetchMyAPI();
             setShowMovies(true);
     }
-
 
     return(
         <div className="movieSearch">
